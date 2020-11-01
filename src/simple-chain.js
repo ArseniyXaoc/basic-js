5d74197eec63a06e5c32ef13a794b246c58f307a
ArseniyXaoc <xaoc190789@gmail.com>
2020-11-01 11:11:53 +0300

    feat: chaining

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,24 +1,40 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
-  getLength() {
-    throw new CustomError('Not implemented');
+  chain: '',
+  separator: '~~',
+  getLength(chain) {
+   return chain.split(this.separator).length;
     // remove line with error and write your code here
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
+    this.chain += (this.chain !== '') ? `${this.separator}( ${value} )` : `( ${value} )`;
+    return this;
     // remove line with error and write your code here
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
+    let chArr = this.chain.split(this.separator);
+    let positionCheck = position-1;
+    if (typeof chArr[positionCheck] === 'undefined' || typeof positionCheck !== 'number') {
+      this.chain = '';
+      throw Error;
+    }
+    chArr.splice(positionCheck, 1);
+    this.chain = chArr.join(this.separator);
+    return this;    
     // remove line with error and write your code here
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
+    let chArr = this.chain.split(this.separator);
+    let reverse = chArr.reverse();
+    this.chain = reverse.join(this.separator);
+    return this;
     // remove line with error and write your code here
   },
   finishChain() {
-    throw new CustomError('Not implemented');
+   let chReturn = this.chain;
+   this.chain = '';
+   return chReturn;
     // remove line with error and write your code here
   }
 };
